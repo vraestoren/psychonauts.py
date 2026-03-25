db2e4629144bc787e36720ab25a6e95f6acbc4ee
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-25 11:47:24 +0300

    Update psychonauts.py

--- src/psychonauts.py
+++ src/psychonauts.py
@@ -7,23 +7,21 @@ class Psychonauts:
         self.session.headers = {
             "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
         }
-    
+
+    def _get(self, endpoint: str) -> dict:
+        return self.session.get(f"{self.api}{endpoint}").json()
+
     def get_all_characters(self, limit: int = 10) -> dict:
-        return self.session.get(
-            f"{self.api}/characters?limit={limit}").json()
-        
+        return self._get(f"/characters?limit={limit}")
+
     def get_character_by_name(self, name: str) -> dict:
-        return self.session.get(
-            f"{self.api}/characters?name={name}").json()
-    
+        return self._get(f"/characters?name={name}")
+
     def get_character_by_gender(self, gender: str) -> dict:
-        return self.session.get(
-            f"{self.api}/characters?gender={gender}").json()
-        
+        return self._get(f"/characters?gender={gender}")
+
     def get_all_psi_powers(self, limit: int = 10) -> dict:
-        return self.session.get(
-            f"{self.api}/powers?limit={limit}").json()
-    
+        return self._get(f"/powers?limit={limit}")
+
     def get_psi_power_by_name(self, name: str) -> dict:
-        return self.session.get(
-            f"{self.api}/powers?name={name}").json()
+        return self._get(f"/powers?name={name}")json()
