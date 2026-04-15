28a19b0ba6c13fac02e6442a0ec33f21306d3c20
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-15 11:34:41 +0300

    Update psychonauts.py

--- src/psychonauts.py
+++ src/psychonauts.py
@@ -1,27 +1,32 @@
 from requests import Session
 
 class Psychonauts:
-    def __init__(self) -> None:
-        self.api = "https://psychonauts-api.herokuapp.com/api"
-        self.session = Session()
-        self.session.headers = {
-            "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
-        }
+	def __init__(self) -> None:
+		self.api = "https://psychonauts-api.herokuapp.com/api"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
+		}
 
-    def _get(self, endpoint: str) -> dict:
-        return self.session.get(f"{self.api}{endpoint}").json()
+	def _get(self, endpoint: str, params: dict = None) -> dict:
+		return self.session.get(f"{self.api}{endpoint}", params=params or {}).json()
 
-    def get_all_characters(self, limit: int = 10) -> dict:
-        return self._get(f"/characters?limit={limit}")
+	def get_all_characters(self, limit: int = 10) -> dict:
+		params = {"limit": limit}
+		return self._get("/characters", params)
 
-    def get_character_by_name(self, name: str) -> dict:
-        return self._get(f"/characters?name={name}")
+	def get_character_by_name(self, name: str) -> dict:
+		params = {"name": name}
+		return self._get("/characters", params)
 
-    def get_character_by_gender(self, gender: str) -> dict:
-        return self._get(f"/characters?gender={gender}")
+	def get_character_by_gender(self, gender: str) -> dict:
+		params = {"gender": gender}
+		return self._get("/characters", params)
 
-    def get_all_psi_powers(self, limit: int = 10) -> dict:
-        return self._get(f"/powers?limit={limit}")
+	def get_all_psi_powers(self, limit: int = 10) -> dict:
+		params = {"limit": limit}
+		return self._get("/powers", params)
 
-    def get_psi_power_by_name(self, name: str) -> dict:
-        return self._get(f"/powers?name={name}")json()
+	def get_psi_power_by_name(self, name: str) -> dict:
+		params = {"name": name}
+		return self._get("/powers", params)
